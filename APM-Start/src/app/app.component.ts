a5c0fcb55792ccd0f806e5364d9e0c1b738934db
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-02-19 17:42:53 +0000

    reacting to router events

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel
+} from "@angular/router";
 
 import { AuthService } from "./user/auth.service";
-import { slideInAnimation } from './app.animation';
+import { slideInAnimation } from "./app.animation";
 
 @Component({
   selector: "pm-root",
@@ -12,6 +19,7 @@ import { slideInAnimation } from './app.animation';
 })
 export class AppComponent {
   pageTitle = "Acme Product Management";
+  loading = true;
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -24,7 +32,24 @@ export class AppComponent {
     return "";
   }
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
 
   logOut(): void {
     this.authService.logout();
